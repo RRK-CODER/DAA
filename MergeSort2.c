e514a1f7d40ca80dd44687dcb806e7fc40075f36
ROHIT RANJAN KUMAR <rohitranjankumar@pec.edu>
2022-06-21 04:40:52 +0530

    add this file

--- MergeSort2.c
+++ MergeSort2.c
@@ -1,35 +1,4 @@
 #include<stdio.h>
- 
-void mergesort(int a[],int low ,int high);
-void merge(int a[],int i1,int j1,int i2,int j2);
- 
-int main()
-{
-int a[30],n,i;
-printf("Enter no of elements:");
-scanf("%d",&n);
-printf("Enter array elements:");
-for(i=0;i<n;i++)
-scanf("%d",&a[i]);
-mergesort(a,0,n-1);
-printf("\nSorted array is :");
-for(i=0;i<n;i++)
-printf("%d ",a[i]);
-return 0;
-}
- 
-void mergesort(int a[],int low,int high)
-{
-int mid;
-if(low<high)
-{
-mid=(low+high)/2;
-mergesort(a,low,mid); //left recursion
-mergesort(a,mid+1,high); //right recursion
-merge(a,low,mid,mid+1,high); //merging of two sorted sub-arrays
-}
-}
- 
 void merge(int a[],int i1,int j1,int i2,int j2)
 {
 int temp[50]; //array used for merging
@@ -52,3 +21,34 @@ temp[k++]=a[high++];
 for(low=i1,high=0;low<=j2;low++,high++)
 a[low]=temp[high];
 }
+ 
+void mergesort(int a[],int low,int high)
+{
+int mid;
+if(low<high)
+{
+mid=(low+high)/2;
+mergesort(a,low,mid); //left recursion
+mergesort(a,mid+1,high); //right recursion
+merge(a,low,mid,mid+1,high); //merging of two sorted sub-arrays
+}
+}
+
+
+int main()
+{
+int a[30],n,i;
+printf("Enter no of elements:");
+scanf("%d",&n);
+printf("Enter array elements:");
+for(i=0;i<n;i++)
+scanf("%d",&a[i]);
+mergesort(a,0,n-1);
+printf("\nSorted array is :");
+for(i=0;i<n;i++)
+printf("%d ",a[i]);
+return 0;
+}
+ 
+
+ 
